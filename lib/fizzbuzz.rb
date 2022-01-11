2bb046b2996738c8d5a174d8082c0e6947afc3da
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-11 16:00:28 +0000

    added additional tests and code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 def fizzbuzz(number)
-  'fizz'
+  if (number % 3).zero? && (number % 5).zero?
+    'fizzbuzz'
+  elsif (number % 3).zero?
+    'fizz'
+  elsif (number % 5).zero?
+    'buzz'
+  else
+    number.to_s
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,29 @@
+# frozen_string_literal: true
+
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns fizz when passed 3' do
-      expect(fizzbuzz(3)).to eq 'fizz'
-    end
+  it 'returns fizz when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns fizz when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'returns buzz when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns buzz when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns buzz when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq '7'
+  end
 end
