f6a4398c1d66bdeae455e65595a64f871d6d6ae1
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-11 20:12:01 +0000

    trying fizzbuzz using filter_map

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
+class Integer
+  def fizzbuzz
+    if (self % 3).zero? && (self % 5).zero?
+      'fizzbuzz'
+    elsif (self % 3).zero?
+      'fizz'
+    elsif (self % 5).zero?
+      'buzz'
+    else
+      self.to_s
+    end
+  end
+end
 
 def fizzbuzz(number)
-  if (number % 3).zero? && (number % 5).zero?
-    'fizzbuzz'
-  elsif (number % 3).zero?
-    'fizz'
-  elsif (number % 5).zero?
-    'buzz'
-  else
-    number.to_s
-  end
+
+  output_text = {
+    3 => "fizz",
+    5 => "buzz",
+    15 => "fizzbuzz"
+  }
+
+  output = output_text.filter_map { |key, value| value if (number % key).zero?}.last
+
+  output.nil? ? number.to_s : output
+
 end
